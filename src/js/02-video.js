9f0015ca7c619bfff806555f4bcbdc07f74a92d7
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-07-30 11:11:10 +0300

    termisnal commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// Select with the DOM API
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+// Select with jQuery
+// If multiple elements are selected, it will use the first element.
+const jqueryPlayer = new Vimeo.Player($('iframe'));
+
+// Select with the `<iframe>`’s id
+// Assumes that there is an <iframe id="player1"> on the page.
+const idPlayer = new Vimeo.Player('player1');
