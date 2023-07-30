f1216a9f175d28f03c8aa303002a166bac25bb8d
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-07-30 09:23:12 +0300

    js-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map((url) =>
+    `<li class= "gallery_item">
+        <a class="gallery_link" href="${url.original}">
+            <img class="gallery_image"
+            src="${url.preview}" 
+            alt="${url.description}"
+            />
+        </a>
+    </li>`
+).join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+    new SimpleLightbox (".gallery a", {
+        captions: true,
+        captionsData: "alt",
+        captionPosition: "bottom", 
+        captionDelay: 250,
+    });
